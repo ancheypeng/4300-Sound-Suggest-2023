cdb24d9571e671af0a2d55c5f8936bc5f278a8b7
Kelvin <klw248@cornell.edu>
2023-03-27 11:43:40 -0400

    db name

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "songsdb"
+MYSQL_DATABASE = "kardashiandb"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
@@ -67,4 +67,4 @@ def episodes_search():
     return sql_search(text)
 
 
-# app.run(debug=True)
+app.run(debug=True)
