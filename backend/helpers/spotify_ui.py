d599411dfcd9b14528a2c87e98eaf25cc4c5f0a4
Fjordrunner <ghoststriker@outlook.com>
2023-04-15 18:36:03 -0400

    New working helper function that gets Spotify URLs

--- backend/helpers/spotify_ui.py
+++ backend/helpers/spotify_ui.py
@@ -1,5 +1,4 @@
 import spotipy
-import json
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sound_suggest_id = '0afed0e0b39e417f8c8603792a5c70c1'
@@ -10,4 +9,22 @@ spotify_object = spotipy.Spotify(client_credentials_manager = client_credentials
 
 def retrieve_spotify_data_for_frontend(query_song : str, query_artist: str):
     retrieved_results = spotify_object.search(q = "artist:" + query_artist + " track:" + query_artist, limit = 1, type = "track")
+    result_data = retrieved_results['tracks']['items'][0]
+
+    song_url = result_data['external_urls']['spotify']
+
+    cover_art_url = result_data['album']['images'][0]['url']
+
+    artist_url = []
+    artists = result_data['artists'] 
+    if len(artists) == 1:
+        artist_url = artists[0]['external_urls']['spotify']
+    else:
+        for artist_info in artists:
+            artist_url.append(artist_info['external_urls']['spotify'])
+
+    return song_url, cover_art_url, artist_url 
+
+
+
     
