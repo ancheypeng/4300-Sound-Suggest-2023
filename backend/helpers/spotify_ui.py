97ddbbc13bea603ddf952bdcf5b9789860d6b639
Fjordrunner <ghoststriker@outlook.com>
2023-04-15 23:15:10 -0400

    Spotify API function with dict output format

--- backend/helpers/spotify_ui.py
+++ backend/helpers/spotify_ui.py
@@ -8,22 +8,31 @@ client_credentials_manager = SpotifyClientCredentials(client_id=sound_suggest_id
 spotify_object = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 def retrieve_spotify_data_for_frontend(query_song : str, query_artist: str):
-    retrieved_results = spotify_object.search(q = "artist:" + query_artist + " track:" + query_artist, limit = 1, type = "track")
+    spotify_result_dict = dict()
+
+    retrieved_results = spotify_object.search(q = "artist:" + query_artist + " track:" + query_song, limit = 1, type = "track")
     result_data = retrieved_results['tracks']['items'][0]
+
+    song_title = result_data['name']
 
     song_url = result_data['external_urls']['spotify']
 
     cover_art_url = result_data['album']['images'][0]['url']
 
-    artist_url = []
-    artists = result_data['artists'] 
-    if len(artists) == 1:
-        artist_url = artists[0]['external_urls']['spotify']
-    else:
-        for artist_info in artists:
-            artist_url.append(artist_info['external_urls']['spotify'])
+    artist_to_url_dict = dict()
+    artists_array = result_data['artists'] 
 
-    return song_url, cover_art_url, artist_url 
+    for artist in artists_array:
+        artist_name = artist['name']
+        artist_homepage = artist['external_urls']['spotify']
+        artist_to_url_dict[artist_name] = artist_homepage
+
+    spotify_result_dict['song_title'] = song_title
+    spotify_result_dict['song_link'] = song_url
+    spotify_result_dict['cover_art'] = cover_art_url
+    spotify_result_dict['artists_links'] = artist_to_url_dict
+
+    return spotify_result_dict
 
 
 
