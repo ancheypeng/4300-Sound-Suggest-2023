fa188934b6588694f513e7526aa9fd382cf01133
Anchey Peng <ancheypeng@gmail.com>
2023-04-16 02:26:25 -0400

    big updates to ui

--- backend/static/script.js
+++ backend/static/script.js
@@ -5,16 +5,18 @@ const suggBox = searchWrapper.querySelector('.autocom-box');
 const icon = searchWrapper.querySelector('.icon');
 let linkTag = searchWrapper.querySelector('a');
 let webLink;
+
 // if user press any key and release
 inputBox.onkeyup = (e) => {
+  if (e.keyCode === 13) {
+    query();
+    return;
+  }
+
   let userData = e.target.value; //user enetered data
+
   let emptyArray = [];
   if (userData) {
-    icon.onclick = () => {
-      webLink = `https://www.google.com/search?q=${userData}`;
-      linkTag.setAttribute('href', webLink);
-      linkTag.click();
-    };
     emptyArray = suggestions.filter((data) => {
       //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
       return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
@@ -38,12 +40,8 @@ inputBox.onkeyup = (e) => {
 function select(element) {
   let selectData = element.textContent;
   inputBox.value = selectData;
-  icon.onclick = () => {
-    webLink = `https://www.google.com/search?q=${selectData}`;
-    linkTag.setAttribute('href', webLink);
-    linkTag.click();
-  };
   searchWrapper.classList.remove('active');
+  inputBox.focus();
 }
 
 function showSuggestions(list) {
@@ -57,6 +55,87 @@ function showSuggestions(list) {
   suggBox.innerHTML = listData;
 }
 
-$('#e1').select2({
+function query() {
+  $('.content').addClass('active-state');
+  console.log('Querying...');
+  song = 'Life Is Good (feat. Drake)';
+  link = 'https://open.spotify.com/track/5yY9lUy8nbvjM1Uyo1Uqoc';
+  thumbnail =
+    'https://i.scdn.co/image/ab67616d0000b2738a01c7b77a34378a62f46402';
+  artists = [
+    {
+      name: 'Future',
+      link: 'https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i',
+    },
+    {
+      name: 'Drake',
+      link: 'https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4',
+    },
+  ];
+  $('.results').append(card(song, link, thumbnail, artists));
+}
+
+$('#social-tags').select2({
   theme: 'bootstrap-5',
+  width: $(this).data('width')
+    ? $(this).data('width')
+    : $(this).hasClass('w-100')
+    ? '100%'
+    : 'style',
+  placeholder: $(this).data('placeholder'),
+  closeOnSelect: false,
 });
+
+// suggestions.forEach((song) => {
+//   let tempOption = document.createElement('option');
+//   tempOption.innerHTML = song;
+//   $('#search').append(tempOption);
+// });
+
+function artistContent(artists) {
+  let artistDiv = $(document.createElement('div'));
+  artists.forEach((artist, i) => {
+    if (i > 0) {
+      artistDiv.html(artistDiv.html() + ', ');
+    }
+    let name = artist['name'];
+    let link = artist['link'];
+    let linkAnchor = $('<a></a>')
+      .attr('href', link)
+      .attr('target', '_blank')
+      .append(`<span class="artist-name">${name}</span>`);
+    artistDiv.append(linkAnchor);
+  });
+
+  return artistDiv.html();
+}
+
+function card(song, link, thumbnail, artists) {
+  let artistsHTML = artistContent(artists);
+  let temp = `
+  <div class="card">
+    <div class="row g-0">
+      <div class="col-md-auto">
+        <img
+          class="cover-art"
+          src="${thumbnail}"
+          alt="Cover Art"
+        />
+      </div>
+      <div class="col-md-auto">
+        <h5 class="song-title">${song}</h5>
+        <p class="artists">
+        ${artistsHTML}
+        </p>
+      </div>
+    </div>
+    <img class="play" src="static/images/play.svg" alt="Play Button" />
+    <a
+      href="${link}"
+      target="_blank"
+      class="stretched-link"
+    ></a>
+  </div>`;
+
+  return temp;
+}
